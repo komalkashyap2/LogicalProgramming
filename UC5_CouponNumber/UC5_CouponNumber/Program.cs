14f218fff83a1951fbebfea3b630f205998acb10
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-03 10:17:40 +0530

    [KOMAL KASHYAP] Added Coupon Number program

---
+++ UC5_CouponNumber/UC5_CouponNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC5_CouponNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            var stringChars = new char[15];
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+                Console.WriteLine(" Coupon Number " + stringChars[i]);
+
+            }
+        }
+    }
+}
